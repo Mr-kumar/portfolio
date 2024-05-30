0da43704e92286d8fed6f798d3df42bc06d53c5d
MANISH KUMAR <106107845+Mr-kumar@users.noreply.github.com>
2024-05-30 23:30:07 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
             transition: { delay: 2.4, duration: 0.6, ease: "easeIn" },
           }}
         >
-          <h2 className="text-4xl font-bold mb-8 text-center">Let's Work Together</h2>
+          <h2 className="text-4xl font-bold mb-8 text-center">Let&apos;s Work Together</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="flex flex-col p-6 bg-gray-800 shadow-lg rounded-lg">
               <h3 className="text-2xl font-bold mb-4 text-accent text-center">
